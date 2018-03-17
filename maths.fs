a2442149af801ebe64f0c997011f2fbec0d74b49
MatthewJohnHeath <matthew.john.heath@gmail.com>
2018-03-17 17:16:58 +0000

    First commit

---
+++ maths.fs
@@ -0,0 +1,23 @@
+﻿namespace FactorGraph
+module Maths=
+    let rebracketR mapPairToSomething =
+        mapPairToSomething 
+            |> Map.toSeq 
+            |> Seq.map (fun ((a,b),c) -> a,(b,c) )
+            |> Map.ofSeq
+
+    let rebracketL mapSomethingToPair =
+        mapSomethingToPair
+            |> Map.toSeq 
+            |> Seq.map (fun (a,(b,c)) -> (a,b) ,c ) 
+            |> Map.ofSeq
+
+    let mult (dic1: Map<int,float>) (dic2: Map<int,float>) =
+        dic1 |> Seq.zip dic2
+        |> Seq.map (fun ( KeyValue(key1,val1),KeyValue(_,val2) ) -> key1, val1*val2 )
+        |> Map.ofSeq
+
+    let plus (dic1: Map<int,float>) (dic2: Map<int,float>) =
+        dic1 |> Seq.zip dic2
+        |> Seq.map (fun ( KeyValue(key1,val1),KeyValue(_,val2) ) -> key1, val1+val2 )
+        |> Map.ofSeq
